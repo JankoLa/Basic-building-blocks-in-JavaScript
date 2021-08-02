35b665569c56124b5990232cc57fe23a35b400f0
JankoLa <jan.laurovic@gmail.com>
2021-08-02 18:30:38 +0200

    solution

---
+++ script.js
@@ -0,0 +1,31 @@
+let firstname = "Ján"
+let surname = "Laurovič"
+let fullname = (firstname + " " + surname)
+console.log(fullname)
+
+let myVariable = true;
+if (myVariable === true) {
+  console.log('The condition has been evaluated to true');
+}
+
+for (let b = 1; b < 21; b++)
+console.log(b)
+
+for (let c = 1; c < 16; c++)
+console.log(c)
+
+
+let greeting = "Hello"
+console.log (greeting+ " " + firstname)
+
+
+let printValues = [0, 3, 6, 7, 9];
+for(let i = 0; i < printValues.length; i++){ 
+console.log(printValues[i]);
+}
+
+
+let myNumbers = [10, 20, 30, 50, 12];
+myNumbers.forEach(number => {
+    console.log(number);
+});
